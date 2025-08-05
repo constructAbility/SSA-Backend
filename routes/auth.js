85b06d350e84b3b24c8bfe6105adc2be220b6ffb
Surjeet <backendoffice12@gmail.com>
2025-08-05 19:01:58 +0530

    hf

--- routes/auth.js
+++ routes/auth.js
@@ -1,9 +1,9 @@
 const express = require('express');
 const router = express.Router();
-const { register, login , registerAdmin,adminlogin} = require('../controllers/authCont');
+const { register, login } = require('../controllers/authCont');
 
 router.post('/register', register);
 router.post('/login', login);
-router.post('/admin/register', registerAdmin); 
-router.post('/admin/login',adminlogin)
+// router.post('/admin/register', registerAdmin); 
+// router.post('/admin/login',adminlogin)
 module.exports = router;
